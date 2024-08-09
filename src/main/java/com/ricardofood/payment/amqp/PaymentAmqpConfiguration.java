f0d0e48c1679d1ea9894cb27562a29e8583888ed
ricardo <ricardo85x@gmail.com>
2024-08-09 10:54:29 -0300

    feat: create payment.completed queue
    Also start it on spring boot

---
+++ src/main/java/com/ricardofood/payment/amqp/PaymentAmqpConfiguration.java
@@ -0,0 +1,29 @@
+package com.ricardofood.payment.amqp;
+
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.QueueBuilder;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class PaymentAmqpConfiguration {
+
+    @Bean
+    public Queue createQueue() {
+        return QueueBuilder.nonDurable("payment.completed").build();
+    }
+
+    @Bean
+    public RabbitAdmin createRabbitAdmin(ConnectionFactory conn) {
+        return new RabbitAdmin(conn);
+    }
+
+    @Bean
+    public ApplicationListener<ApplicationReadyEvent> startRabbitAdmin(RabbitAdmin rabbitAdmin) {
+        return event -> rabbitAdmin.initialize();
+    }
+}
